046ca9396fb68c3db848ae689e887b9405c45b88
Vikaras <arturasch@gmail.com>
2018-05-22 01:57:56 +0300

    Started modifying calendar

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,15 @@ $(function() {
 
     $('#calendar').fullCalendar({
         // put your options and callbacks here
+        height: 752,
+        contentHeight: 702,
+        fixedWeekCount: false, //savaiciu kiekis
+        header:
+            {
+                left: 'month,basicDay',
+                center: 'prev,title,next',
+                right: ''
+            }
     })
 
 });
